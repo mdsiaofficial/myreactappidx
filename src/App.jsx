b6832b9fb2fa0f6743f9757ddc123db015831841
rvdashique <rvdashique@gmail.com>
2024-05-07 11:06:37 +0000

    bg changer

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Card from './Card'
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("red");
 
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{bgColor: color}} className="w-full h-screen duration">
+
+        <div className="fixed flex justify-center bottom-12 inset-x-0 px-2 flex-wrap">
+          <div className="flex flex-wrap justify-center shadow-xl gap-3 bg-white px-3 py-2 rounded-xl">Test</div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+export default function Card(){
+
+  return (
+    <>
+      <div className="bg-red-500">
+        <p>My Card</p>
+        <img src="https://picsum.photos/200/300" alt="" />
+      </div>
+    </>
+  )
+}
